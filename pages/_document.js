6a069252eba8879f0502c4ef3eafe9d9005e3561
paghdjob <51829174+paghdjob@users.noreply.github.com>
2021-09-13 20:52:56 +0530

    Add files via upload
    added

--- pages/_document.js
+++ pages/_document.js
@@ -24,14 +24,32 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <body>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-140663137-1`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-140663137-1', {
+          page_path: window.location.pathname,
+        });
+      `,
+            }}
+          />
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P6STXZL"
         height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             }}
           />
+        </Head>
+        <body>
           <Main />
           <NextScript />
         </body>
