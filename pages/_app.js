815704b405da2c1809f5674793300e47f8c54e78
yogesh <yogesh@MacBook-Air.local>
2021-10-24 13:59:54 +0530

    wc3

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import Script from 'next/script'
+// import Head from "next/head";
+// import Script from 'next/script'
 // import "../styles/globals.css";
 import "../styles/bootstrap.min.css";
 import "../styles/globals.css";
@@ -10,12 +10,12 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-      {/* <Script
+      {/* <Head>
+       <Script
         src="https://www.googletagmanager.com/gtag/js?id=UA-140663137-1"
         strategy="lazyOnload"
-      /> */}        
-      </Head>
+      />      
+      </Head> */}   
       <Component {...pageProps} />
     </>
   );
